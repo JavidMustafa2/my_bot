0e329e761de0b3c1cf9e5fef7af7851b7291b8e2
Javid Mustafa <jmustafa2011@hotmail.com>
2025-07-07 10:07:57 -0500

    added camera sensor to the roborg

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -30,12 +30,6 @@ def generate_launch_description():
     )
 
 
-    joint_state_publisher_gui_node = Node(
-    package='joint_state_publisher_gui',
-    executable='joint_state_publisher_gui',
-    name='joint_state_publisher_gui',
-    output='screen'
-    )
 
     # Launch!
     return LaunchDescription([
@@ -44,6 +38,5 @@ def generate_launch_description():
             default_value='false',
             description='Use sim time if true'),
 
-        joint_state_publisher_gui_node,
         node_robot_state_publisher
     ])
